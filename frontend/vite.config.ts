b55b028ef29d8a645513576a9490fdcc3bd91927
hnevesg <helena.neves@alu.uclm.es>
2025-05-12 12:46:58 +0200

    SSL Certificate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', 
-    port: 80,
+    port: 443,
+    https: {
+      key: fs.readFileSync('./certs/privkey.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    },
     allowedHosts: [
       'gkstatsweb.com',  
     ],
